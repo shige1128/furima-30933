aa0fb600a9cfac9df11ed4e345d8569abf858a90
shige1128 <shigedeen@gmail.com>
2021-01-15 15:25:21 +0900

    バリデーション、ストロングパラメータ設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday,  presence: true
+  with_options presence: true do
+    validates :family_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "は、カタカナで入力して下さい" }
+    validates :family_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は、'ひらがな'、'カタカナ'、'漢字'で入力して下さい" }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root "projects#index"
+  resources :projects, only: [:index, :new, :destroy]
 end
