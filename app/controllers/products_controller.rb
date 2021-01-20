012852fc58501d7cedf80a0dd304383660105da4
shige1128 <shigedeen@gmail.com>
2021-01-20 14:03:06 +0900

    コントローラ、ルーティング修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,17 @@
 class ProductsController < ApplicationController
+  def index
+  end
+  
+  def new
+    @product = Product.new
+  end
+
+  def create
+  end
 
   private
 
   def product_params
     params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "projects#index"
-  resources :projects, only: [:index, :new]
+  root "products#index"
+  resources :products, only: [:index, :new, :create]
 end
