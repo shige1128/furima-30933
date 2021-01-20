be7f9d5107082420728cbe8c10846408768b96e8
shige1128 <shigedeen@gmail.com>
2021-01-20 14:05:21 +0900

    ActiveHash導入

--- db/migrate/20210118131115_create_products.rb
+++ db/migrate/20210118131115_create_products.rb
@@ -1,14 +1,14 @@
 class CreateProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :products do |t|
-      t.string :products_name
-      t.text :text
-      t.integer :category_id
-      t.integer :status_id
-      t.integer :shipping_id
-      t.integer :area_id
-      t.integer :days_id
-      t.integer :price_id
+      t.string :products_name , null: false
+      t.text :text            , null: false
+      t.integer :category_id  , null: false
+      t.integer :status_id    , null: false
+      t.integer :shipping_id  , null: false
+      t.integer :area_id      , null: false
+      t.integer :days_id      , null: false
+      t.integer :price_id     , null: false
       t.integer :user_id, foreign_key: true
       t.timestamps
     end
