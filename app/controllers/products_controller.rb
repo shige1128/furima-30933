57aa4e4a451cd0cfa2540cd74b548e65ab46fda6
shige1128 <shigedeen@gmail.com>
2021-01-26 15:28:40 +0900

    記述をまとめる

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :destroy]
+  before_action :set_product, only: [:show, :destroy]
 
   def index
     @products = Product.order('created_at DESC')
@@ -19,12 +20,10 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def destroy
-    product = Product.find(params[:id])
-    if current_user.id == product.user.id
+    if current_user.id == @product.user.id
       product.destroy
       redirect_to root_path
     else
@@ -33,6 +32,10 @@ class ProductsController < ApplicationController
   end
 
   private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
 
   def product_params
     params.require(:product).permit(:image, :products_name, :text, :category_id, :status_id, :shipping_id, :area_id, :day_id,
