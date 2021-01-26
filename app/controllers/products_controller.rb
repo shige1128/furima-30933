a8e9d26cc2bc1563eb306f9d73c3fec13a885f92
shige1128 <shigedeen@gmail.com>
2021-01-26 13:14:41 +0900

    レビュー修正(エラーハンドリング)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,8 +24,12 @@ class ProductsController < ApplicationController
 
   def destroy
     product = Product.find(params[:id])
-    product.destroy
-    redirect_to root_path
+    if current_user.id == product.user.id
+      product.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
 
   private
