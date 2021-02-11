882d335491c6f006c28f126e4afbc50536d5fbd6
shige1128 <shigedeen@gmail.com>
2021-02-11 18:25:53 +0900

    モデル、コントローラー編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,17 +1,17 @@
 class OrdersController < ApplicationController
+  before_action :set_product, only: [:index, :create]
 
   def index
-    @product = Product.find(params[:product_id])
     @order_history = OrderHistory.new
   end
 
   def create
     @order_history = OrderHistory.new(order_params)
     if @order_history.valid?
+      pay_product
       @order_history.save
       redirect_to root_path
     else
-      @product = Product.find(params[:product_id])
       render :index
     end
   end
@@ -19,7 +19,20 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_history).permit(:postal_code, :area, :municipalities, :address_number, :building, :phone_number).merge(user_id: current_user.id, product_id: params[:product_id])
+    params.require(:order_history).permit(:postal_code, :area, :municipalities, :address_number, :building, :phone_number).merge(user_id: current_user.id, product_id: params[:product_id], token: params[:token])
+  end
+
+  def set_product
+    @product = Product.find(params[:product_id])
+  end
+
+  def pay_product
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @product.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 
 end
